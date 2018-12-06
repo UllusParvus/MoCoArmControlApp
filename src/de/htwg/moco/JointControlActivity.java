d7408ac69dfb183d67331d1c02a98cd9fd0cb505
beschaef <beschaef@htwg-konstanz.de>
2018-12-06 15:31:24 +0100

    added calibration support, temporary reuse gripper button

--- src/de/htwg/moco/JointControlActivity.java
+++ src/de/htwg/moco/JointControlActivity.java
@@ -1,16 +1,18 @@
 package de.htwg.moco;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.Toast;
+
 import org.ros.android.RosActivity;
 import org.ros.node.NodeConfiguration;
 import org.ros.node.NodeMainExecutor;
 import java.net.URI;
-
 
 /**
  * @author ch721ulr@htwg-konstanz.de (Christoph Ulrich)
@@ -18,95 +20,113 @@ import java.net.URI;
  */
 public class JointControlActivity extends RosActivity {
 
-  private JointTalker nxt_1_talker;
-  private JointTalker nxt_2_talker;
+    private JointTalker nxt_1_talker;
+    private JointTalker nxt_2_talker;
+    private double calibrate = 0.0;
+    private boolean finished = false;
 
-  // TODO: Title still in work. Just changed the tutorial title
-  public JointControlActivity() {
+    // TODO: Title still in work. Just changed the tutorial title
+    public JointControlActivity() {
     //specify the master uri of your roscore machine here as last param
-    super("RosMindStormManipulator", "RosMindStormManipulator", URI.create("http://10.42.0.1:11311"));
-  }
+        super("RosMindStormManipulator", "RosMindStormManipulator", URI.create("http://10.42.0.1:11311"));
+    }
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    requestWindowFeature(Window.FEATURE_NO_TITLE);
-    getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
-    setContentView(R.layout.activity_joint_control);
-    findViewById(R.id.buttonJointControl);
-
-    Button b_joint_0_p, b_joint_0_m, b_joint_1_p, b_joint_1_m,b_joint_2_p,b_joint_2_m,b_gripper_p,b_gripper_m;
-    b_joint_0_p = findViewById(R.id.b_joint_0_p);
-    b_joint_0_m = findViewById(R.id.b_joint_0_m);
-    b_joint_1_p = findViewById(R.id.b_joint_1_p);
-    b_joint_1_m = findViewById(R.id.b_joint_1_m);
-    b_joint_2_p = findViewById(R.id.b_joint_2_p);
-    b_joint_2_m = findViewById(R.id.b_joint_2_m);
-    b_gripper_p = findViewById(R.id.b_gripper_p);
-    b_gripper_m = findViewById(R.id.b_gripper_m);
-    b_joint_0_p.setId(R.id.b_joint_0_p);
-    b_joint_0_m.setId(R.id.b_joint_0_m);
-    b_joint_1_p.setId(R.id.b_joint_1_p);
-    b_joint_1_m.setId(R.id.b_joint_1_m);
-    b_joint_2_p.setId(R.id.b_joint_2_p);
-    b_joint_2_m.setId(R.id.b_joint_2_m);
-    b_gripper_p.setId(R.id.b_gripper_p);
-    b_gripper_m.setId(R.id.b_gripper_m);
-    MyTouchListener touchListener = new MyTouchListener();
-    b_joint_0_p.setOnTouchListener(touchListener);
-    b_joint_0_m.setOnTouchListener(touchListener);
-    b_joint_1_p.setOnTouchListener(touchListener);
-    b_joint_1_m.setOnTouchListener(touchListener);
-    b_joint_2_p.setOnTouchListener(touchListener);
-    b_joint_2_m.setOnTouchListener(touchListener);
-    b_gripper_p.setOnTouchListener(touchListener);
-    b_gripper_m.setOnTouchListener(touchListener);
-
-
-  }
-
-  public class MyTouchListener implements View.OnTouchListener {
     @Override
-    public boolean onTouch(View v, MotionEvent event) {
-      int motor_positive = 1;
-      String motor_name = "";
-      double motor_effort = 1.5;
-      switch(v.getId()){
-        case R.id.b_joint_0_m:
-          motor_positive = -1;
-        case R.id.b_joint_0_p:
-          motor_name = "motor_1";
-          break;
-        case R.id.b_joint_1_m:
-          motor_positive = -1;
-        case R.id.b_joint_1_p:
-          motor_name = "motor_2";
-          break;
-        case R.id.b_joint_2_m:
-          motor_positive = -1;
-        case R.id.b_joint_2_p:
-          motor_name = "motor_3";
-          motor_effort = 0.6;
-          break;
-        case R.id.b_gripper_m:
-          motor_positive = -1;
-        case R.id.b_gripper_p:
-          motor_name = "motor_1";
-          break;
-      }
-      int action = event.getAction();
-      if (MotionEvent.ACTION_DOWN == action) {
-        v.setPressed(true);
-        nxt_1_talker.loop(motor_effort * motor_positive, motor_name);
-      }
-      else if (MotionEvent.ACTION_UP == action) {
-        v.setPressed(false);
-        nxt_1_talker.loop(0, motor_name);
-      }
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_joint_control);
+        findViewById(R.id.buttonJointControl);
+
+        Button b_joint_0_p, b_joint_0_m, b_joint_1_p, b_joint_1_m,b_joint_2_p,b_joint_2_m,b_gripper_p,b_gripper_m;
+        b_joint_0_p = findViewById(R.id.b_joint_0_p);
+        b_joint_0_m = findViewById(R.id.b_joint_0_m);
+        b_joint_1_p = findViewById(R.id.b_joint_1_p);
+        b_joint_1_m = findViewById(R.id.b_joint_1_m);
+        b_joint_2_p = findViewById(R.id.b_joint_2_p);
+        b_joint_2_m = findViewById(R.id.b_joint_2_m);
+        b_gripper_p = findViewById(R.id.b_gripper_p);
+        b_gripper_m = findViewById(R.id.b_gripper_m);
+        b_joint_0_p.setId(R.id.b_joint_0_p);
+        b_joint_0_m.setId(R.id.b_joint_0_m);
+        b_joint_1_p.setId(R.id.b_joint_1_p);
+        b_joint_1_m.setId(R.id.b_joint_1_m);
+        b_joint_2_p.setId(R.id.b_joint_2_p);
+        b_joint_2_m.setId(R.id.b_joint_2_m);
+        b_gripper_p.setId(R.id.b_gripper_p);
+        b_gripper_m.setId(R.id.b_gripper_m);
+        MyTouchListener touchListener = new MyTouchListener();
+        b_joint_0_p.setOnTouchListener(touchListener);
+        b_joint_0_m.setOnTouchListener(touchListener);
+        b_joint_1_p.setOnTouchListener(touchListener);
+        b_joint_1_m.setOnTouchListener(touchListener);
+        b_joint_2_p.setOnTouchListener(touchListener);
+        b_joint_2_m.setOnTouchListener(touchListener);
+        b_gripper_p.setOnTouchListener(touchListener);
+        b_gripper_m.setOnTouchListener(touchListener);
+
+
+    }
+
+    public class MyTouchListener implements View.OnTouchListener {
+        @Override
+        public boolean onTouch(View v, MotionEvent event) {
+            int motor_positive = 1;
+            String motor_name = "";
+            double motor_effort = 1.5;
+            switch(v.getId()){
+                case R.id.b_joint_0_m:
+                    motor_positive = -1;
+                case R.id.b_joint_0_p:
+                    motor_name = "motor_1";
+                    break;
+                case R.id.b_joint_1_m:
+                    motor_positive = -1;
+                case R.id.b_joint_1_p:
+                    motor_name = "motor_2";
+                    break;
+                case R.id.b_joint_2_m:
+                    motor_positive = -1;
+                case R.id.b_joint_2_p:
+                    motor_name = "motor_3";
+                    motor_effort = 0.6;
+                    break;
+                case R.id.b_gripper_m:
+                    while (!finished) {
+                        double range = nxt_1_talker.get_range();
+                        Log.i("RANGE ", ""+range);
+                        if (range > .185) {
+                            nxt_1_talker.loop(-0.6, "motor_2");
+                        }
+                        if (range < .195) {
+                            nxt_1_talker.loop(0.6, "motor_2");
+                        }
+                        if ((range <= .195) && (range >= .185)) {
+                            finished = true;
+                            nxt_1_talker.loop(0.0, "motor_2");
+                        }
+                    }
+                    finished = false;
+                    return true;
+                case R.id.b_gripper_p:
+                    break;
+        }
+        int action = event.getAction();
+        if (MotionEvent.ACTION_DOWN == action) {
+            v.setPressed(true);
+            nxt_1_talker.loop(motor_effort * motor_positive, motor_name);
+        }
+        else if (MotionEvent.ACTION_UP == action) {
+            v.setPressed(false);
+            nxt_1_talker.loop(0, motor_name);
+        }
+
+
       return true;
     }
 
-}
+  }
 
   @Override
   protected void init(NodeMainExecutor nodeMainExecutor) {
--- src/de/htwg/moco/JointTalker.java
+++ src/de/htwg/moco/JointTalker.java
@@ -1,15 +1,24 @@
 package de.htwg.moco;
 
+import android.util.Log;
+
+import org.ros.message.MessageListener;
 import org.ros.namespace.GraphName;
 import org.ros.node.AbstractNodeMain;
 import org.ros.node.ConnectedNode;
 import org.ros.node.topic.Publisher;
+import org.ros.node.topic.Subscriber;
+
 import nxt_msgs.JointCommand;
+import nxt_msgs.Range;
 
 public class JointTalker extends AbstractNodeMain {
     private String topic_name;
 
     private Publisher<JointCommand> joint_publisher;
+    private Subscriber<Range> ultrasonic_sub;
+
+    private Range us_range;
 
     public JointTalker() {
         this.topic_name = "/nxt_1/joint_command";
@@ -24,7 +33,15 @@ public class JointTalker extends AbstractNodeMain {
     }
 
     public void onStart(ConnectedNode connectedNode) {
-        joint_publisher = connectedNode.newPublisher(this.topic_name, "nxt_msgs/JointCommand"); // TODO: change publisher message to nxt message
+        joint_publisher = connectedNode.newPublisher(this.topic_name, "nxt_msgs/JointCommand");
+        ultrasonic_sub = connectedNode.newSubscriber("/ultrasonic_sensor", "nxt_msgs/Range");
+        ultrasonic_sub.addMessageListener(new MessageListener<Range>() {
+            @Override
+            public void onNewMessage(Range range) {
+                us_range = range;
+                Log.i("Sub" , "got range " + us_range.getRange() );
+            }
+        }, 1);
 
     }
 
@@ -34,6 +51,12 @@ public class JointTalker extends AbstractNodeMain {
         nxt_msg.setEffort(effort);
         nxt_msg.setName(motor_name);
         joint_publisher.publish(nxt_msg);
+
+    }
+
+    public double get_range() {
+        return us_range.getRange();
+
     }
 }
 
